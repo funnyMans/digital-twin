e7e8a734f9cf542f5f715e9f00795524a23bb03b
Vladimir <vl.petrosyan94@gmail.com>
2025-12-28 14:27:25 +0400

    auth handler fix

--- app/api/auth/[...all]/route.ts
+++ app/api/auth/[...all]/route.ts
@@ -1,4 +1,12 @@
 import { auth } from "@/app/lib/auth/auth";
 import { toNextJsHandler } from "better-auth/next-js";
 
-export const { POST, GET } = toNextJsHandler(auth);
+const handler = toNextJsHandler(auth);
+
+export async function GET(req: Request) {
+  return handler.GET ? handler.GET(req) : new Response(null, { status: 405 });
+}
+
+export async function POST(req: Request) {
+  return handler.POST ? handler.POST(req) : new Response(null, { status: 405 });
+}
