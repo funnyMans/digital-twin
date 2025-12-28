a792e9c83d0fd30ac64d71fb46dcde66646da031
Vladimir <vl.petrosyan94@gmail.com>
2025-12-28 15:22:20 +0400

    fix/ fastforward to vercel host

--- app/lib/auth/auth-client.ts
+++ app/lib/auth/auth-client.ts
@@ -1,6 +1,8 @@
 import { createAuthClient } from "better-auth/react";
+const baseURL = process.env.NEXT_PUBLIC_BETTER_AUTH_URL;
+
 export const authClient = createAuthClient({
-  baseURL: "http://localhost:3000",
+  baseURL,
 });
 
 export const { signIn, signUp, useSession } = authClient;
